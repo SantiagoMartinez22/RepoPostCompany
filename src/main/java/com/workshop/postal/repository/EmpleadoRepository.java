6fca957f74a85080689aaf81944c8493d4dcc4a5
SantiagoMartinez22 <smyardev2@gmail.com>
2023-09-08 20:33:50 -0500

    solucion y correcion en donde se añadieron 2 metodos a la interfaz

--- src/main/java/com/workshop/postal/repository/EmpleadoRepository.java
+++ src/main/java/com/workshop/postal/repository/EmpleadoRepository.java
@@ -3,6 +3,12 @@ package com.workshop.postal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.workshop.postal.models.Empleado;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
 
+    Optional<Empleado> findById(UUID id);
+
+    void deleteById(UUID id);
 }
